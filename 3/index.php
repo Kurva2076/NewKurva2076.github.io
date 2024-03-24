21af2bb30126ca415c77c77721b88d201fed7463
Kurva2076 <85439926+Kurva2076@users.noreply.github.com>
2024-03-25 01:16:28 +0300

    Update index.php

--- 3/index.php
+++ 3/index.php
@@ -2,7 +2,6 @@
 
 header('Content-Type: text/html; charset=UTF-8');
 
-
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     include("form.php");
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -15,22 +14,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $program_ls = [];
 
     try {
-        $db = new PDO('mysql:host=localhost;dbname=u67319', $username, $password);
-        $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $db -> setAttribute(PDO::ATTR_PERSISTENT, true);
+        $db = new PDO($dsn, $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_PERSISTENT, true);
 
-        $langs_stmt = $db -> query('SELECT * FROM Programming_Languages');
-        while ($row = $langs_stmt -> fetch(PDO::FETCH_ASSOC)) {
-            array_push($program_ls, $row['language_name']);
+        $lang_stmt = $db->query('SELECT * FROM Programming_Languages');
+        while ($row = $lang_stmt->fetch(PDO::FETCH_ASSOC)) {
+            $program_ls[] = $row['language_name'];
         }
 
         unset($db);
     } catch (PDOException $e) {
-        echo ' Ошибка подключения: ' . $e->getMessage();
+        echo 'Ошибка подключения к серверу: ' . $e->getMessage();
+        exit();
     }
 
-    print_r($program_ls);
-    
     $required_fields = [
         'fullName' => [
             'Empty' => 'ФИО',
@@ -69,7 +67,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     ];
     $system_message = '';
 
-    // Проверка полей на пустоту
     foreach ($required_fields as $name => $message) {
         if (empty($_POST[$name]) and $name != 'agreement') {
             $system_message = ($name == 'sex') ? 'Укажите ' :
@@ -80,20 +77,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         }
     }
 
-    // Проверка полей на корретность заполнения
     if ($system_message == '') {
-        // Проверка содержимого programLanguages
         $is_programLanguages_correctly = true;
         foreach ($_POST['programLanguages'] as $language)
             if (!in_array($language, $program_ls, true))
                 $is_programLanguages_correctly = false;
 
-        if (preg_match("/[^a-zа-яё ]|\s$|\s\s/ui", $_POST['fullName']) or
-            strlen($_POST['fullName']) > 150) {
+        if (preg_match("/[^a-zа-яё ]|\s$|\s\s/ui", $_POST['fullName']) or strlen($_POST['fullName']) > 150) {
             $system_message = $required_fields['fullName']['notCorrectly'];
         } elseif (preg_match("/[^0-9+ ]|\s$|\s\s|\+{2,}|(?<!^)\+/", $_POST['phoneNumber']) or
-            !preg_match("/\s/", $_POST['phoneNumber']) or
-            strlen($_POST['phoneNumber']) > 30) {
+            !preg_match("/\s/", $_POST['phoneNumber']) or strlen($_POST['phoneNumber']) > 30) {
             $system_message = $required_fields['phoneNumber']['notCorrectly'];
         } elseif (!preg_match("/^[a-zA-Z0-9_\-.]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/", $_POST['email'])) {
             $system_message = $required_fields['email']['notCorrectly'];
@@ -110,31 +103,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         } elseif (preg_match("/^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]+$/", $_POST['birth'])) {
             $date = array_map('intval', explode('.', $_POST['birth']));
 
-            // Проверка существования введённой даты
-            if ($date[0] < 1 or $date[0] > 31 or $date[1] < 1 or $date[1] > 12 or $date[2] < 1)
+            if ($date[0] < 1 or $date[0] > 31 or $date[1] < 1 or $date[1] > 12 or $date[2] < 1) {
                 $system_message = $required_fields['birth']['notCorrectly'];
-            elseif ($date[2] % 4 != 0 and $date[1] == 2 and $date[0] > 28)
+            } elseif ($date[2] % 4 != 0 and $date[1] == 2 and $date[0] > 28) {
                 $system_message = $required_fields['birth']['notCorrectly'];
-            elseif ($date[2] % 4 == 0 and $date[1] == 2 and $date[0] > 29)
+            } elseif ($date[2] % 4 == 0 and $date[1] == 2 and $date[0] > 29) {
                 $system_message = $required_fields['birth']['notCorrectly'];
-            elseif ($date[1] > 1 and $date[1] < 7 and $date[1] % 2 == 0 and $date[0] > 30)
+            } elseif ($date[1] > 1 and $date[1] < 7 and $date[1] % 2 == 0 and $date[0] > 30) {
                 $system_message = $required_fields['birth']['notCorrectly'];
-            elseif ($date[1] > 7 and $date[1] < 12 and $date[1] % 2 == 1 and $date[0] > 30)
+            } elseif ($date[1] > 7 and $date[1] < 12 and $date[1] % 2 == 1 and $date[0] > 30) {
                 $system_message = $required_fields['birth']['notCorrectly'];
+            }
 
-            // Сравнивание с текущей датой
             if ($system_message == '') {
                 $cur_date = array_map('intval', explode('/', date('m/d/Y')));
 
                 if ($date[2] > $cur_date[2] or
                     $date[2] == $cur_date[2] and $date[1] > $cur_date[1] or
-                    $date[2] == $cur_date[2] and $date[1] == $cur_date[1] and $date[0] > $cur_date[0])
+                    $date[2] == $cur_date[2] and $date[1] == $cur_date[1] and $date[0] > $cur_date[0]) {
                     $system_message = $required_fields['birth']['notCorrectly'];
-                elseif ($date[2] > $cur_date[2] - 14 or $date[2] < $cur_date[2] - 100)
+                } elseif ($date[2] > $cur_date[2] - 14 or $date[2] < $cur_date[2] - 100) {
                     $system_message = $required_fields['birth']['outOfRange'];
+                }
             }
         }
-
     }
 
     if ($system_message != '') {
@@ -142,64 +134,60 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         exit();
     } else {
         echo "Все поля заполнены корректно. Отправляем данные на сервер...";
-        print_r($_POST);
     }
 
     try {
-        $db = new PDO('mysql:host=localhost;dbname=u67319', $username, $password);
-        $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $db -> setAttribute(PDO::ATTR_PERSISTENT, true);
+        $db = new PDO($dsn, $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_PERSISTENT, true);
 
-        echo ' Успешное подключение к базе данных.';
+        echo ' Успешное подключение к базе данных. ';
 
         $datebirth = explode('.', $_POST['birth']);
         $datebirth = $datebirth[2] . '-' . $datebirth[1] . '-' . $datebirth[0];
         $agreement = (strcmp($_POST['agreement'], 'on') == 0) ? 1 : 0;
-        
-        $users_stmt = $db -> prepare(
+
+        $users_stmt = $db->prepare(
             "INSERT INTO Users (full_name,datebirth,sex,phone_number,e_mail,biography,agreement) 
             VALUES (:full_name,:datebirth,:sex,:phone_number,:e_mail,:biography,:agreement)"
         );
-        $users_stmt -> bindParam(':full_name', $_POST['fullName']);
-        $users_stmt -> bindParam(':datebirth', $datebirth);
-        $users_stmt -> bindParam(':sex', $_POST['sex']);
-        $users_stmt -> bindParam(':phone_number', $_POST['phoneNumber']);
-        $users_stmt -> bindParam(':e_mail', $_POST['email']);
-        $users_stmt -> bindParam(':biography', $_POST['bio']);
-        $users_stmt -> bindParam(':agreement', $agreement);
-        $users_stmt -> execute();
+        $users_stmt->bindParam(':full_name', $_POST['fullName']);
+        $users_stmt->bindParam(':datebirth', $datebirth);
+        $users_stmt->bindParam(':sex', $_POST['sex']);
+        $users_stmt->bindParam(':phone_number', $_POST['phoneNumber']);
+        $users_stmt->bindParam(':e_mail', $_POST['email']);
+        $users_stmt->bindParam(':biography', $_POST['bio']);
+        $users_stmt->bindParam(':agreement', $agreement);
+        $users_stmt->execute();
 
-        $user_stmt = $db -> query(
-            "SELECT user_id FROM Users WHERE user_id = LAST_INSERT_ID()"
-        );
-        $user_id = $user_stmt -> fetch(PDO::FETCH_ASSOC)['user_id'];
-        echo $user_id;
+        $user_stmt = $db->query("SELECT user_id FROM Users WHERE user_id = LAST_INSERT_ID()");
+        $user_id = $user_stmt->fetch(PDO::FETCH_ASSOC)['user_id'];
 
-        $langs = [];
+        $lang = [];
         foreach ($_POST['programLanguages'] as $language)
-            $langs[$language] = 0;
-        
-        $langs_stmt = $db -> query('SELECT * FROM Programming_Languages');
-        while ($row = $langs_stmt -> fetch(PDO::FETCH_ASSOC)) {
-            if (isset($langs[$row['language_name']])) {
-                 $langs[$row['language_name']] = $row['language_id'];
+            $lang[$language] = 0;
+
+        $lang_stmt = $db->query('SELECT * FROM Programming_Languages');
+        while ($row = $lang_stmt->fetch(PDO::FETCH_ASSOC)) {
+            if (isset($lang[$row['language_name']])) {
+                $lang[$row['language_name']] = $row['language_id'];
             }
         }
 
-        print_r($langs);
-
-        $users_program_langs_stmt = $db -> prepare(
+        $users_program_lang_stmt = $db->prepare(
             "INSERT INTO Users_Programming_Languages (user_id,language_id) VALUES (:user_id,:language_id)"
         );
-        foreach ($langs as $language_name => $language_id) {
-            echo $language_name . " " . $language_id . " ";
-            $users_program_langs_stmt -> bindParam(':user_id', $user_id);
-            $users_program_langs_stmt -> bindParam(':language_id', $language_id);
-            $users_program_langs_stmt -> execute();
+        foreach ($lang as $language_name => $language_id) {
+            $users_program_lang_stmt->bindParam(':user_id', $user_id);
+            $users_program_lang_stmt->bindParam(':language_id', $language_id);
+            $users_program_lang_stmt->execute();
         }
+
+        echo "Данные успешно сохранены.";
 
         unset($db);
     } catch (PDOException $e) {
-        echo ' Ошибка подключения: ' . $e->getMessage();
+        echo ' Ошибка связи с сервером: ' . $e->getMessage();
+        exit();
     }
 }
